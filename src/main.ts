b03e7d463cde85b91bdbe0f5cb53510148e7625d
lukas.fritsch <lukas.fritsch@outlook.com>
2020-09-09 22:53:42 +0200

    Add logger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { Players } from "w3ts/globals";
 import { addScriptHook, W3TS_HOOK } from "w3ts/hooks";
 import { PremadeUnits } from "units/units";
 import { getActivePlayers } from "players";
+import { Log, LogLevel } from "utils/Serilog/Serilog";
+import { StringSink } from "utils/Serilog/Sinks/StringSink";
 
 const BUILD_DATE = compiletime(() => new Date().toUTCString());
 const TS_VERSION = compiletime(() => require("typescript").version);
 const TSTL_VERSION = compiletime(() => require("typescript-to-lua").version);
+
+//Log.Init([new StringSink(LogLevel.Debug, )]);
 
 // setup vars
 
